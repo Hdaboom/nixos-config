a422ef04e47f6ad774e471df98a48dc1ed0b88c3
hdaboom <hdaboom@gmail.com>
2026-03-03 17:02:01 +0000

    added vscodium extensions and defined user settings

--- modules/home-manager/programs/vscodium.nix
+++ modules/home-manager/programs/vscodium.nix
@@ -6,11 +6,16 @@
     package = pkgs.vscodium.fhs;
 
     extensions = with pkgs.vscode-extensions; [
-
+      bbenoist.nix
+      enkia.tokyo-night
+      ms-vscode.hexeditor
     ];
 
     userSettings = {
       "nix.enableLanguageServer" = true;
+      "workbench.colorTheme" = "Tokyo Night Storm";
+      "editor.autoIndentOnPaste" = true;
+      "files.autoSave" = "afterDelay";
     };
   };
 }
